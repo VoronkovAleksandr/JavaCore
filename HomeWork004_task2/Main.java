6906ba8dbd051304422932137ac20a6e2fd90c14
VoronkovAleksandr <a_voron@list.ru>
2023-11-01 21:44:15 +0500

    HomeWork004 task2 completed

--- HomeWork004_task2/Main.java
+++ HomeWork004_task2/Main.java
@@ -5,61 +5,89 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static List<Buyer> buyers;
+    static List<Product> products;
+    static List<Order> orders;
+
     public static void main(String[] args) {
 
-        List<Buyer> buyers = new ArrayList();
-        List<Product> products = new ArrayList();
-        List<Order> orders = new ArrayList();
+        buyers = new ArrayList();
+        products = new ArrayList();
+        orders = new ArrayList();
 
         fillShop(buyers, products);
 
-        startShopping(buyers, products, orders);
-        System.out.printf("Всего совершено %s покупок", orders.size());
+        startShopping();
+
+        System.out.printf("Всего совершено покупок: %s", orders.size());
     }
 
-    private static void startShopping(List<Buyer> buyers, List<Product> products, List<Order> orders) throws NoBuyerException, NoProductException {
+    private static void startShopping() {
         Scanner scanner = new Scanner(System.in);
         boolean start = true;
 
         while (start) {
-            Buyer buyer = null;
-            Product product = null;
+
 
             System.out.println("Введите ник покупателя:");
             String nickName = scanner.nextLine();
-
-            for (Buyer item: buyers
-                 ) {
-                if (item.getNickName().equals(nickName)) {
-                    buyer = item;
-                    break;
-                }
-            }
-            if (buyer == null) throw new NoBuyerException(nickName);
-
             System.out.println("Введите наименование товара:");
             String productName = scanner.nextLine();
-            for (Product item: products
-                 ) {
-                if (item.getProductName().equals(productName)){
-                    product = item;
-                    break;
-                }
-            }
-            if (product == null) throw new NoProductException(productName);
-
-
             System.out.println("Введите количество товара:");
             String quantity = scanner.nextLine();
-
-
-            System.out.println("Чтобы завершить покупки введите 0");
+            try {
+                makePurchase(nickName, productName, quantity);
+            } catch (NoProductException e) {
+                System.out.println("Такого продукта нет, повторите ввод");
+                ;
+            } catch (NoBuyerException e) {
+                System.out.println("Такого покупателя нет, повторите ввод");
+                ;
+            } catch (NegativeNumberException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("Чтобы завершить покупки введите 0, чтобы продолжить покупки нажмите ENTER");
             if (scanner.nextLine().equals("0")) start = false;
         }
     }
 
-    private static void makePurchase(String buyer, String product, String quantity) {
+    private static void makePurchase(String buyer, String product, String quantity) throws NoProductException, NoBuyerException, NegativeNumberException {
+        Product setProduct = null;
+        Buyer buyer_ = getBuyer(buyer);
+        Product product_ = getProduct(product);
+        Integer quantity_ = getInteger(quantity);
+        if (buyer_ == null) throw new NoBuyerException(buyer);
+        if (product_ == null) throw new NoProductException(product);
+        if (quantity_ == null) throw new NegativeNumberException("Вы ввели 0 или отрицательное число");
 
+        orders.add(new Order(buyer_, product_, quantity_));
+    }
+
+    private static Integer getInteger(String number) throws NegativeNumberException {
+        try {
+            Integer num = Integer.parseInt(number);
+            if (num > 0) return num;
+        } catch (NumberFormatException e){
+            throw new NegativeNumberException("Вы ввели символ вместо числа");
+        }
+        return null;
+    }
+
+    private static Product getProduct(String product) {
+        for (Product item : products
+        ) {
+            if (item.getProductName().equals(product)) return item;
+        }
+        return null;
+    }
+
+
+    private static Buyer getBuyer(String nickName) {
+        for (Buyer item : buyers
+        ) {
+            if (item.getNickName().equals(nickName)) return item;
+        }
+        return null;
     }
 
     private static void fillShop(List<Buyer> buyers, List<Product> products) {
---
+++ HomeWork004_task2/NegativeNumberException.java
@@ -0,0 +1,7 @@
+package HomeWork004_task2;
+
+public class NegativeNumberException extends Exception {
+    public NegativeNumberException(String message) {
+        super(message);
+    }
+}
